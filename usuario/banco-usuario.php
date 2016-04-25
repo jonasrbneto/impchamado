809130469aa83940e7a6677b91c3b3dbb970ef53
PH83 <phfmu2014@gmail.com>
2016-04-25 16:25:10 -0300

    05-01

--- usuario/banco-usuario.php
+++ usuario/banco-usuario.php
@@ -4,7 +4,8 @@
 
 function insereUsuario( $conexao, $login, $senha, $nome, $email, $perfil){
 	$query = "INSERT INTO `usuario` ( `login`, `senha`, `nome`, `email`, `perfil`) VALUES ('${login}', '${senha}', '${nome}', '${email}', '${perfil}')";
-	$resultadoDaInsercao = mysqli_query($conexao, $query);	
+	$resultadoDaInsercao = mysqli_query($conexao, $query);
+	return $resultadoDaInsercao;	
 }
 
 function existeUsuario($conexao,$login, $senha){
@@ -15,6 +16,22 @@ function existeUsuario($conexao,$login, $senha){
 	return $usuario;
 }
 
+function listaUsuarios($conexao) {
+    $usuarios = array();
+    $query = "select * from usuario";
+    $resultado = mysqli_query($conexao, $query);
+
+    while($usuario = mysqli_fetch_assoc($resultado)) {
+        array_push($usuarios, $usuario);
+    }
+    return $usuarios;
+}
+
+function apagarUsuario($conexao, $id){
+    $query = "DELETE FROM usuario WHERE idLogin = '${id}'";
+    $resultadoDoDelete = mysqli_query($conexao, $query);
+    return $resultadoDoDelete;
+}
 
 
 
