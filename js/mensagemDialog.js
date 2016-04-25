692505edc1ba22a41e8ff064ff96d6954eec94c2
PH83 <phfmu2014@gmail.com>
2016-04-25 16:29:21 -0300

    05-04

--- js/mensagemDialog.js
+++ js/mensagemDialog.js
@@ -8,4 +8,24 @@ function mostraMsgErro(idTablePosition,MSG){
 
 } 
 
+function mostraMsgConfirm(idTablePosition,MSG,url){
+
+	$("#mensagemConfirma").html(MSG).css("color", "black");
+	$( "#divMensagemConfirma" ).dialog({
+      modal:true,
+      width:440,
+      title:"Confirm",
+      position: { my: "top", at: "top center", of: $("#"+idTablePosition)},
+      buttons: {
+        "Sim": function() {
+          window.location.href = url;;
+        },
+        Cancel: function() {
+          $( this ).dialog( "close" );
+          
+        }
+      }
+    });
+} 
+
 
